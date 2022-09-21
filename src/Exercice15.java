59164059be599be75feee5339260117e7e9dbbb4
alu10211999 <alumar1999@ieselcaminas.org>
2022-09-21 19:17:28 +0200

    Second commit

--- src/Exercice15.java
+++ src/Exercice15.java
@@ -11,15 +11,15 @@ public class Exercice15 {
 
         if ( 0 <= x && x < 3) {
             System.out.println("Muy deficiente");
-        } if ( 3 <= x && x < 5) {
+        } else if ( 3 <= x && x < 5) {
             System.out.println("Insuficiente");
-        } if ( 5 <= x && x < 6) {
+        } else if ( x == 5 ) {
             System.out.println("Suficiente");
-        } if ( 6 <= x && x < 7 ) {
+        } else if ( x == 6 ) {
             System.out.println("Bien");
-        } if ( 7 <= x && x < 9 ) {
+        } else if ( 7 <= x && x < 9 ) {
             System.out.println("Notable");
-        } if ( 9 <= x && x <= 10) {
+        } else {
             System.out.println("Sobresaliente");
         }
     }
